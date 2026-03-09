f1f83c814735a84bbd9086a9e81da0322b7c4b93
Fábio M. Costa <fmc@ufg.br>
2026-03-09 07:16:16 -0300

    1st example

---
+++ ThreadCtrl.java
@@ -0,0 +1,28 @@
+public class ThreadCtrl extends Thread {
+   public static int amount = 0;
+   public static void main(String[] args) {
+       ThreadCtrl thread = new ThreadCtrl();
+       thread.start();
+       int copyAmount = amount;
+       System.out.println("Main: " + copyAmount);
+       amount = increment();
+       System.out.println("Main: " + amount);
+   }
+   public void run() {
+       System.out.println("2nd Thread: " + amount);
+       amount = increment();
+       System.out.println("2nd Thread: " + amount);
+   }
+   public static synchronized int increment(){
+       int newAmount = amount;
+       try {
+           // Pause
+           Thread.sleep(500);
+       } catch (InterruptedException e) {
+           // Handle the case where another thread interrupts the sleep
+           System.out.println("Sleep was interrupted: " + e.getMessage());
+           Thread.currentThread().interrupt(); // Restore interrupt status
+       }
+       return newAmount+1;
+   }
+}
