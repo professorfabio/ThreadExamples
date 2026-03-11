a216a0ab2faa8afadf69e5459c8a1a76edca8654
Fábio M. Costa <fmc@ufg.br>
2026-03-11 11:22:24 -0300

    More examples

--- Ex02_MinhaThreadRunnable.java
+++ Ex02_MinhaThreadRunnable.java
@@ -7,15 +7,15 @@ public class Ex02_MinhaThreadRunnable implements Runnable{
     public void run(){
         System.out.println("Início: " + this.nome);
         for(int i=0; i<5; i++){
-            System.out.println(i + "-" + this.nome);
+            System.out.println(i + " - " + this.nome);
         }
         System.out.println("Fim: " + this.nome);
     }
     public static void main(String[] args){
-        Ex02_MinhaThreadRunnable minhaThread1 = new Ex02_MinhaThreadRunnable("MinhaThread1");
-        Ex02_MinhaThreadRunnable minhaThread2 = new Ex02_MinhaThreadRunnable("MinhaThread2");
-        Thread thread1 = new Thread(minhaThread1, minhaThread1.nome);
-        Thread thread2 = new Thread(minhaThread2, minhaThread2.nome);
+        Ex02_MinhaThreadRunnable runnable1 = new Ex02_MinhaThreadRunnable("MinhaThread1");
+        Ex02_MinhaThreadRunnable runnable2 = new Ex02_MinhaThreadRunnable("MinhaThread2");
+        Thread thread1 = new Thread(runnable1, runnable2.nome);
+        Thread thread2 = new Thread(runnable2, runnable2.nome);
         thread1.start();
         thread2.start();
     }
